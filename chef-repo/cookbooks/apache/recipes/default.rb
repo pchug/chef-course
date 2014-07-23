6f24b4f42dfdac5e7c06cd9a7a05ef279f5b09b3
Piyush Chugh <piyush.chugh@gmail.com>
2014-07-23 10:27:09 -0400

    Looping thru clowns and bears

--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -1,28 +1,47 @@
-#
-# Cookbook Name:: apache
-# Recipe:: default
-#
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
 
 package 'httpd' do
   action	:install
 end
-
-# template '' do
-#
-#end
 
 service 'httpd' do
   action [ :enable,
 	   :start ]
 end
 
-node.default['apache']['indexfile'] = 'index2.html'
+src_file = "/etc/httpd/conf.d/welcome.conf"
 
-cookbook_file '/var/www/html/index.html' do
-  source node['apache']['indexfile']
-  mode '0644'
+execute "mv #{src_file} #{src_file}.disabled" do
+  only_if do
+    File.exists?(src_file)
+  end
+  notifies :restart, "service[httpd]"
+end
+
+# iterate over apache sites
+node['apache']['sites'].each do |site_name, site_data|
+  document_root = "/srv/apache/#{site_name}"
+
+  template "/etc/httpd/conf.d/#{site_name}.conf" do
+    source "custom.erb"
+    mode   "0644"
+    variables( :document_root => document_root,
+               :port => site_data['port'],
+             )
+    notifies :restart, "service[httpd]"
+  end
+
+  directory document_root do
+    mode    "0755"
+    recursive true
+  end
+
+  template "#{document_root}/index.html" do
+    source "index.html.erb"
+    mode "0644"
+    variables(
+      :site_name => site_name,
+      :port => site_data['port'],
+    )
+  end
+
 end
