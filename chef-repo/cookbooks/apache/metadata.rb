b3feb8c7bf67325c580c5d9dfd3a6304349db728
Piyush Chugh <piyush.chugh@gmail.com>
2014-07-23 10:26:25 -0400

    added dependency

--- chef-repo/cookbooks/apache/metadata.rb
+++ chef-repo/cookbooks/apache/metadata.rb
@@ -5,3 +5,6 @@ license          'All rights reserved'
 description      'Installs/Configures apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
+
+depends "motd"
+
