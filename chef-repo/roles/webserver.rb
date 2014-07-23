eb85476255e428d9dc12ba560231dc55a7240ed1
Piyush Chugh <piyush.chugh@gmail.com>
2014-07-23 14:12:06 -0400

    new web server role defined

---
+++ chef-repo/roles/webserver.rb
@@ -0,0 +1,11 @@
+name "webserver"
+description "Web Server"
+run_list "recipe[apache]"
+
+default_attributes({
+  "apache" => {
+    "sites" => {
+      "admin" => {
+        "port" => 8000
+}}}})
+
